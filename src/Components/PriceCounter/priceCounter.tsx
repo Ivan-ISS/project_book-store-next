c68427557b733ea4fda1c0b2266ef3a85afa733f
Ivan-ISS <vanya5@inbox.ru>
2024-04-30 10:01:56 +0500

    project_book-store-next: added definition of current currency code

--- src/Components/PriceCounter/priceCounter.tsx
+++ src/Components/PriceCounter/priceCounter.tsx
@@ -6,6 +6,7 @@ import formatToPrice from '@/utils/formatToPrice';
 
 export default function PriceCounter() {
     const [ total, setTotal ] = useState<number>(0);
+    const [ currencyCode, setCurrencyCode ] = useState<string | null>(null);
     const booksInBag = useSelector((state: RootState) => state.auth.bag);
 
     useEffect(() => {
@@ -13,6 +14,7 @@ export default function PriceCounter() {
         booksInBag.map((bookInbag) => {
             if (bookInbag.retailPrice) {
                 summ = summ + bookInbag.retailPrice.amount * bookInbag.quantity;
+                setCurrencyCode(bookInbag.retailPrice.currencyCode);
             }
         });
         setTotal(summ);
@@ -20,7 +22,7 @@ export default function PriceCounter() {
 
     return (
         <p className={styles.totalPrice}>
-            {`Total Price: ${formatToPrice(total.toFixed(2))} RUB`}
+            {`Total Price: ${formatToPrice(total.toFixed(2))} ${currencyCode ? currencyCode : ''}`}
         </p>
     );
 }
